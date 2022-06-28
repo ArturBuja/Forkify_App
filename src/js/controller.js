ca3af86c93d4b2f1373f56f4d0cc483240006c6d
Bobeer95 <artur.buja2@gmail.com>
2022-06-28 12:03:08 +0200

    wyświetlanie listy przepisów

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,7 @@
 import * as model from './model.js';
 import recipieView from './views/recipieView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView';
 
 //scripts
 import 'core-js/stable';
@@ -33,6 +34,8 @@ const controlRecipes = async function (e) {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // 1) Get seatch quert
     const query = searchView.getQuery();
     if (!query) return;
@@ -42,6 +45,7 @@ const controlSearchResults = async function () {
 
     // 3) render results
     console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
